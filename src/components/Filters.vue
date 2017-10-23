21ac9704e658d33401d48aa616211f4772f7e1d3
Vitaliy Bykovets <vitaliy.bykovets@selectoglobal.com>
2017-10-23 18:18:14 +0300

    chore: start to add responsive styles

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -4,36 +4,34 @@
       <input class="text-field" type="text" placeholder="Пошук..." @input="changeFilter($event.target.value)">
     </div>
 
-    <div class="filter__group flex">
-      <div class="filters__box flex flex--center">
-        <div class="radio"
-             v-for="(amountType, index) in weightFilters">
-          <input type="radio"
-                 name="wight"
-                 :id="'weight-' + index"
-                 :checked="activeWeight === amountType.value"
-                 @change="changeWeight(amountType.value)">
-          <label :for="'weight-' + index">{{ amountType.name }}</label>
-        </div>
+    <div class="filters__box flex flex--center">
+      <div class="radio"
+           v-for="(amountType, index) in weightFilters">
+        <input type="radio"
+               name="wight"
+               :id="'weight-' + index"
+               :checked="activeWeight === amountType.value"
+               @change="changeWeight(amountType.value)">
+        <label :for="'weight-' + index">{{ amountType.name }}</label>
       </div>
+    </div>
 
-      <div class="filters__box">
-        <div class="select">
-          <button class="select__active-item">
-            <span>Сортування</span>
-            <i class="icon icon-arrow-down"></i>
-            <i class="icon icon-arrow-up"></i>
-          </button>
-          <ul class="select__list">
-            <li class="select__list-item"
-                v-for="sort in sortFilters"
-                :class="{ 'is-active' : sort.value === activeSortType }"
-                v-bind:key="sort.value"
-                v-on:click="changeFilter(sort.value)">
-              {{ sort.name }}
-            </li>
-          </ul>
-        </div>
+    <div class="filters__box">
+      <div class="select">
+        <button class="select__active-item">
+          <span>Сортування</span>
+          <i class="icon icon-arrow-down"></i>
+          <i class="icon icon-arrow-up"></i>
+        </button>
+        <ul class="select__list">
+          <li class="select__list-item"
+              v-for="sort in sortFilters"
+              :class="{ 'is-active' : sort.value === activeSortType }"
+              v-bind:key="sort.value"
+              v-on:click="changeFilter(sort.value)">
+            {{ sort.name }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
