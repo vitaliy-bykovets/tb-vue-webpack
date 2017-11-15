90b6976f922fe56fc29fb6d33237b02aff1dcfdb
Vitaliy Bykovets <vitaliy.bykovets@selectoglobal.com>
2017-11-15 21:04:06 +0200

    fix: disable history mode

--- src/routers/index.js
+++ src/routers/index.js
@@ -6,6 +6,7 @@ import Home from '../components/Home.vue';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     { path: '/', name: 'home', component: Home },
     { path: '*', redirect: { name: 'home' } },
